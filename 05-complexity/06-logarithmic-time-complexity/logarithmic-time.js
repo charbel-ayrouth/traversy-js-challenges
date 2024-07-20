7b54157be980e7f99ed373288344dbbd1e4a1856
charbel ayrouth <ayrouthcharbel@gmail.com>
2024-07-20 12:10:21 +0300

    logarithmic time

--- 05-complexity/06-logarithmic-time-complexity/logarithmic-time.js
+++ 05-complexity/06-logarithmic-time-complexity/logarithmic-time.js
@@ -5,3 +5,26 @@ Logarithmic time means that the time required to complete a function is proporti
 
 */
 
+// logarithmic is most of the time when dividing by 2
+
+function findPower(base, exponent) {
+  if (exponent === 0) {
+    return 1;
+  }
+
+  if (exponent % 2 === 0) {
+    const halfPower = findPower(base, exponent / 2);
+    return halfPower * halfPower;
+  } else {
+    const halfPower = findPower(base, (exponent - 1) / 2);
+    return base * halfPower * halfPower;
+  }
+}
+
+console.time("Find Power 1");
+findPower(2, 100);
+console.timeEnd("Find Power 1");
+
+console.time("Find Power 2");
+findPower(2, 1000000000);
+console.timeEnd("Find Power 2");
